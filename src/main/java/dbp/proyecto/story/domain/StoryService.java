4a8c586f5575f8b930114656c72ad8d9a8444408
joaquinsalinas06 <joaquin.salinas@utec.edu.pe>
2024-05-15 14:59:35 -0500

    Validacion de repositorio usuario

--- src/main/java/dbp/proyecto/story/domain/StoryService.java
+++ src/main/java/dbp/proyecto/story/domain/StoryService.java
@@ -5,6 +5,8 @@ import dbp.proyecto.story.dto.StoryPatchDTO;
 import dbp.proyecto.story.dto.StoryResponseDTO;
 import dbp.proyecto.story.infrastructure.StoryRepository;
 import dbp.proyecto.user.domain.User;
+import dbp.proyecto.user.dto.UserInfoForSong;
+import dbp.proyecto.user.infrastructure.UserRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
@@ -14,10 +16,13 @@ import java.util.Objects;
 public class StoryService {
     private final StoryRepository storyRepository;
 
+    private final UserRepository userRepository;
+
     private final ModelMapper modelMapper;
 
-    public StoryService(StoryRepository storyRepository, ModelMapper modelMapper) {
+    public StoryService(StoryRepository storyRepository, UserRepository userRepository, ModelMapper modelMapper) {
         this.storyRepository = storyRepository;
+        this.userRepository = userRepository;
         this.modelMapper = modelMapper;
     }
 
@@ -26,8 +31,8 @@ public class StoryService {
         return modelMapper.map(story, StoryResponseDTO.class);
     }
 
-    public StoryResponseDTO getStoryByAuthor(User author, Long id) {
-        //todo check if author is the owner of the story
+    public StoryResponseDTO getStoryByAuthor(UserInfoForSong author, Long id) {
+        User user = userRepository.findById(author.getId()).orElseThrow(() -> new ResourceNotFoundException("Author not found"));
 
         Story story = storyRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Story not found"));
         if (!Objects.equals(story.getUser().getId(), author.getId())) {

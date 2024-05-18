3019ec0d3b239ab7ea02673d1037a362f3321398
_Aldebaran_ <santiago.cama@utec.edu.pe>
2024-05-18 14:04:20 -0500

    Arregloxd

--- src/main/java/dbp/proyecto/playlistSongs/PlaylistSongsRepository.java
+++
@@ -1,6 +0,0 @@
-package dbp.proyecto.playlistSongs;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PlaylistSongsRepository extends JpaRepository<PlaylistSongs, PlaylistSongId> {
-}

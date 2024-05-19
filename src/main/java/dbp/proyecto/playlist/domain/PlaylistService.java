b558cd5140213032656517e00220fec2115fac64
joaquinsalinas06 <joaquin.salinas@utec.edu.pe>
2024-05-19 00:04:22 -0500

    Quitar funciones antiguas

--- src/main/java/dbp/proyecto/playlist/domain/PlaylistService.java
+++ src/main/java/dbp/proyecto/playlist/domain/PlaylistService.java
@@ -1,27 +1,22 @@
 package dbp.proyecto.playlist.domain;
 
+
+import dbp.proyecto.playlist.dtos.PlaylistResponseDTO;
 import dbp.proyecto.song.domain.Song;
 import dbp.proyecto.user.domain.User;
-import dbp.proyecto.playlist.dtos.PlaylistResponseDTO;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+
 @Service
 public class PlaylistService {
+
     public List<PlaylistResponseDTO> getPlaylistByName(String name) {
         return null;
     }
 
     public PlaylistResponseDTO getPlaylistById(Long id) {
-        return null;
-    }
-
-    public PlaylistResponseDTO getPlaylistByAuthors(User users) {
-        return null;
-    }
-
-    public PlaylistResponseDTO getPlaylistBySong(Song song) {
         return null;
     }
 

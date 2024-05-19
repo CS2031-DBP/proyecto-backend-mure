54a44210a28aa2dcb225df833aa5d26066d9f71d
KarTaGo124 <joseguillermogalvezpacori@gmail.com>
2024-05-19 18:06:26 -0500

    quitando unused imports

--- src/main/java/dbp/proyecto/user/domain/UserService.java
+++ src/main/java/dbp/proyecto/user/domain/UserService.java
@@ -10,15 +10,12 @@ import dbp.proyecto.user.infrastructure.UserRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import dbp.proyecto.exception.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service

a38c81c7283e887f36efed2ace62b3619db07e1c
KarTaGo124 <joseguillermogalvezpacori@gmail.com>
2024-05-25 05:22:18 -0500

    agregamos h2.database y quitamos contextLoads de MureApplicationTests

--- src/test/java/dbp/proyecto/MureApplicationTests.java
+++ src/test/java/dbp/proyecto/MureApplicationTests.java
@@ -1,12 +1,8 @@
 package dbp.proyecto;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MureApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 }

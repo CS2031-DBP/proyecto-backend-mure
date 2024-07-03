1c4184360a09ccdaa85804d0a0e983ffe526e685
AlejandroEN <alejandroescobarnunez@gmail.com>
2024-07-03 14:42:30 -0500

    Add create_functions sql file

--- src/main/resources/db/create_functions.sql
+++ src/main/resources/db/create_functions.sql
@@ -1,9 +1,9 @@
-CREATE OR REPLACE FUNCTION normalize_text(text)
-RETURNS text AS $$
-BEGIN
-RETURN LOWER(UNACCENT($1));
-END;
-$$ LANGUAGE plpgsql;
 CREATE EXTENSION IF NOT EXISTS unaccent;
 
-DROP FUNCTION IF EXISTS normalize_text(text);
+CREATE OR REPLACE FUNCTION normalize_text(text)
+    RETURNS text AS
+$$
+BEGIN
+    RETURN LOWER(UNACCENT($1));
+END;
+$$ LANGUAGE plpgsql;

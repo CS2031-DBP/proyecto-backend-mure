0a0e35d23ffcc8efdc0e7a1331df7e8d524df086
joaquinsalinas06 <joaquin.salinas@utec.edu.pe>
2024-07-04 15:18:16 -0500

    Cambio en la estructura del post, mejor manejo de los elementos de la cancion y el album

--- src/main/java/dbp/proyecto/post/infrastructure/PostRepository.java
+++ src/main/java/dbp/proyecto/post/infrastructure/PostRepository.java
@@ -1,6 +1,8 @@
 package dbp.proyecto.post.infrastructure;
 
 import dbp.proyecto.post.domain.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -9,4 +11,5 @@ public interface PostRepository extends JpaRepository<Post, Long> {
     List<Post> findBySongId(Long songId);
     List<Post> findByAlbumId(Long albumId);
     List<Post> findByUserId(Long userId);
+    Page<Post> findAllByOrderByCreatedAtDesc(Pageable pageable);
 }
